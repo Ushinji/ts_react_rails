fcb8f5d3aa41296840482ad4315caa52ef3dad30
Ushinji <siguma971@gmail.com>
2019-04-06 00:33:57 +0900

    Add load an asset file in development.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def assets_path(path)
+    return webpack_dev_server_path(path)
+  end
+
+  private
+  def webpack_dev_server_path(path)
+    "http://localhost:3010/#{path}"
+  end
 end
