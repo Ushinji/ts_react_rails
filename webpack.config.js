3ea852704873c7a3c6d2c65566852374abf0cb43
Ushinji <siguma971@gmail.com>
2019-04-06 00:24:55 +0900

    Add setup webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const ManifestPlugin = require("webpack-manifest-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.jsx"
+  },
+  output: {
+    path: path.join(__dirname, "./public/dist"),
+    filename: "[name]-[hash].js",
+    publicPath: `http://localhost:3010/`
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [new ManifestPlugin()],
+  devServer: {
+    contentBase: "../public/dist",
+    port: 3010,
+    disableHostCheck: true,
+    host: "0.0.0.0"
+  }
+};
