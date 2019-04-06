54311191226d1867398f2558eabc8ff23b29b967
Ushinji <siguma971@gmail.com>
2019-04-06 19:17:37 +0900

    Add styled-component support

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,22 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: inline-block;
+  font-size: 18px;
+  border-radius: 8px;
+  text-align: center;
+  text-decoration: none;
+  background-color: #337ab7;
+  color: #ffffff;
+
+  &:hover {
+    text-decoration: none;
+    color: #ffffff;
+    background-color: #286090;
+    cursor: pointer;
+  }
+`;
 
 const Counter: React.FC = () => {
   const [count, SetCount] = React.useState(0);
@@ -10,9 +28,9 @@ const Counter: React.FC = () => {
   return (
     <div>
       <h1>カウンター</h1>
-      <button type="button" onClick={handleClick}>
+      <Button type="button" onClick={handleClick}>
         ＋
-      </button>
+      </Button>
       <div>{count}</div>
     </div>
   );
--- src/testUtils/setup.ts
+++ src/testUtils/setup.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import 'jest-styled-components';
 import { cleanup } from 'react-testing-library';
 
 afterEach(cleanup);
