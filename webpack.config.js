32f4e2f5941309b575526b7db2deb909a49d814c
Ushinji <siguma971@gmail.com>
2019-04-06 21:15:36 +0900

    Fix add trailing connma in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,31 @@ const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: {
-    main: './src/index.tsx'
+    main: './src/index.tsx',
   },
   output: {
     path: path.join(__dirname, './public/dist'),
     filename: '[name]-[hash].js',
     publicPath:
-      process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3010/'
+      process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3010/',
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   module: {
     rules: [
       {
         test: /\.*(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: 'ts-loader'
-      }
-    ]
+        loader: 'ts-loader',
+      },
+    ],
   },
   plugins: [new ManifestPlugin()],
   devServer: {
     contentBase: '../public/dist',
     port: 3010,
     disableHostCheck: true,
-    host: '0.0.0.0'
-  }
+    host: '0.0.0.0',
+  },
 };
