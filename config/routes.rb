03992fe89703920b5dfa70834c5be1ca6cd7bf95
Ushinji <siguma971@gmail.com>
2019-04-19 15:37:08 +0900

    Add API to return Project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    resources :projects, only: [:show]
+  end
+
   match "*any", to: "root#index", via: :all
 end
