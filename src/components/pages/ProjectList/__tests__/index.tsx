a6c8188df94c8e01e26aa7f5cf44f66b48b91fbc
Ushinji <siguma971@gmail.com>
2019-05-04 10:42:38 +0900

    Add test to display  items for projectList page.

--- src/components/pages/ProjectList/__tests__/index.tsx
+++ src/components/pages/ProjectList/__tests__/index.tsx
@@ -1,4 +1,4 @@
-import { Builder, By, Capabilities } from 'selenium-webdriver';
+import { Builder, By, Capabilities, until } from 'selenium-webdriver';
 
 describe('pages/ProjectList', () => {
   it('Display page title', async () => {
@@ -11,6 +11,24 @@ describe('pages/ProjectList', () => {
     const title = await driver.findElement(By.css('h1')).getText();
 
     expect(title).toEqual('プロジェクト一覧');
+
+    await driver.quit();
+  });
+
+  it.only('Display Project list item', async () => {
+    const capabilities = Capabilities.chrome();
+    capabilities.set('chromeOptions', {
+      args: ['--headless', '--no-sandbox', `--window-size=1280,700`],
+    });
+    const driver = await new Builder().withCapabilities(capabilities).build();
+    await driver.get('http://localhost:3000/projects');
+    const elements = await driver.wait(
+      until.elementsLocated(By.css('a')),
+      5000
+    );
+
+    expect(await elements[0].getText()).toEqual('テストプロジェクト１');
+    expect(await elements[1].getText()).toEqual('テストプロジェクト２');
 
     await driver.quit();
   });
